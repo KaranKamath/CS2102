124a6c3d413b5799d37d5346f011226a6c8207e0
Karan Kamath <artemisfowl2.karan@gmail.com>
2013-11-16 13:13:39 +0800

    somewhere

--- htdocs/testconn.php
+++ htdocs/testconn.php
@@ -1,4 +1,18 @@
-<?php
-require "config.php";
-echo "<h1>Success!</h1>";
+<?php 
+require "connect.php";
+
+$name = "Singapore";
+$password = "Mumbai";
+
+$resArray = array();
+
+$result =  mysqli_query($con, "SELECT * FROM $table_name WHERE uid = '$name' and password = '$password'; ") or die("query died");
+
+while($row = mysqli_fetch_array($result)) {
+	array_push($resArray, $row);
+	echo $row['uid'] . " " . $row['password'];
+	echo "<br>";
+}
+
+echo json_encode($resArray);
 ?>
